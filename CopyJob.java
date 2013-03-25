f1b5b15598777188879f855f520afc200a0ebab3
mark @ wxb <mark@silicontrip.net>
2013-03-25 15:29:41 +1100

    Commented out unused item.

--- CopyJob.java
+++ CopyJob.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class CopyJob  implements Runnable, Serializable {
 	
-	public final static String statusCode[] = {"Success","Pending","Copying","Error"};
+//	public final static String statusCode[] = {"Success","Pending","Copying","Error"};
 	public final static int OK = 0;
 	public final static int WAIT = 1;
 	public final static int COPY = 2;
